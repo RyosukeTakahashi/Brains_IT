bcad2ee2a852ac53482f2d3f5b842a292275d157
Ryo Murakami <ramuniku@gmail.com>
2016-06-13 22:58:48 +0900

    no message

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,8 +1,17 @@
-window.addEventListener('load', function () {
-  var contactForm = document.getElementById('contactForm')    
-  contactForm.addEventListener('submit', function () {
-    // ここにバリデーションとか
-    $("#modal-example").modal("show");
-//    milkform.submit();
+//window.addEventListener('load', function () {
+//  var contactForm = document.getElementById('contactForm')    
+//  contactForm.addEventListener('submit', function () {
+//    // ここにバリデーションとか
+//    $("#modal-example").modal("show");
+////    milkform.submit();
+//  });
+//});
+
+$("#contactForm").submit(function(e) {
+  e.preventDefault();
+
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
   });
 });
