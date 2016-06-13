f7f875562d42edf47e80d4897724a097382d53ea
Ryo Murakami <ramuniku@gmail.com>
2016-06-13 23:02:03 +0900

    no message

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,6 +12,6 @@ $("#contactForm").submit(function(e) {
 
   var $form = $(this);
   $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you!");
+    $("#modal-example").modal("show");
   });
 });
