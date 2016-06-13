a425eeaafa812071694e08e7b15bb1f365a57e00
Ryo Murakami <ramuniku@gmail.com>
2016-06-13 22:48:12 +0900

    no message

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,32 +1,8 @@
 window.addEventListener('load', function () {
   var contactForm = document.getElementById('contactForm')    
-  var milkform = new MilkForm('catip5mi4vk', function(isSupported){
-    // ロード完了時処理を書きたい場合はここ
-    // isSupportedにはMilkcocoaにブラウザが対応しているかが帰ってくるので
-    // milkform-fallbackを使わずに、サポートの有無に応じてこちらに自由に処理を書くことも出来ます
-  });
-  milkform.onSuccess(function(datum){
-    //送信成功時処理を書きたい場合はここ
-//      alert("お問い合わせありがとうございます。\nメールが送信されました。")
-      
-      $("#modal-example").modal("show");
-  });
-  milkform.onFailure(function(error){
-    //送信失敗時処理を書きたい場合はここ
-  });
-    
   contactForm.addEventListener('submit', function () {
     // ここにバリデーションとか
-    milkform.submit();
+    $("#modal-example").modal("show");
+//    milkform.submit();
   });
 });
-
-
-
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
-
-//泥臭くvalidation書いて、
